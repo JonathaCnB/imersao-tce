dd6f3c2d2bbb617dcc6b690985f46b67f0c61461
JonathaCnB <jotacarlosftb@gmail.com>
2021-09-09 20:14:01 -0400

    Update: create app user

---
+++ users/models.py
@@ -0,0 +1,55 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from localflavor.br.models import BRCNPJField, BRStateField
+
+
+class User(AbstractUser):
+    cnpj = BRCNPJField(
+        "CNPJ",
+        default="",
+    )
+    state = BRStateField(
+        "Estado",
+        default="",
+    )
+    address = models.CharField(
+        "Endereço",
+        max_length=250,
+        default="",
+    )
+    cell_phone = models.CharField(
+        "Telefone Celular",
+        max_length=250,
+        default="",
+        blank=True,
+    )
+    number = models.CharField(
+        "Número",
+        max_length=10,
+        default="",
+    )
+    complement = models.CharField(
+        "Complemento",
+        max_length=20,
+        default="",
+        blank=True,
+        null=True,
+    )
+    district = models.CharField(
+        "Bairro",
+        max_length=100,
+        default="",
+    )
+    city = models.CharField(
+        "Cidade",
+        max_length=100,
+        default="",
+    )
+
+    class Meta:
+        verbose_name = "Usuário"
+        verbose_name_plural = "Usuários"
+        db_table = "user"
+
+    def __str__(self):
+        return f"{str(self.id)} - {str(self.email)}"
