037deaf2a8b1c89a5cb1d631fe76ba7f1fcda61d
JonathaCnB <jotacarlosftb@gmail.com>
2021-09-14 20:35:46 -0400

    Path: implementação das rotas para filtros

--- users/serializers.py
+++ users/serializers.py
@@ -8,54 +8,22 @@ class UserSerializer(serializers.ModelSerializer):
     name = serializers.SerializerMethodField(read_only=True)
     id = serializers.SerializerMethodField(read_only=True)
     is_admin = serializers.SerializerMethodField(read_only=True)
-
-    class Meta:
-        model = User
-        fields = ["id", "email", "name", "is_admin", "score"]
-
-    def get_id(self, obj):
-        return obj.id
-
-    def get_is_admin(self, obj):
-        return obj.is_staff
-
-    def get_name(self, obj):
-        name = obj.first_name
-        if name == "":
-            name = obj.email
-        return name
-
-
-class UserSerializerWithToken(UserSerializer):
-    token = serializers.SerializerMethodField(read_only=True)
-
-    class Meta:
-        model = User
-        fields = ["id", "username", "email", "name", "is_admin", "token"]
-
-    def get_token(self, obj):
-        token = RefreshToken.for_user(obj)
-        return str(token.access_token)
-
-
-class UserSerializer(serializers.ModelSerializer):
-    name = serializers.SerializerMethodField(read_only=True)
-    id = serializers.SerializerMethodField(read_only=True)
-    is_admin = serializers.SerializerMethodField(read_only=True)
-    state = serializers.SerializerMethodField(read_only=True)
+    state_user = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = User
         fields = [
             "id",
             "name",
+            "first_name",
+            "last_name",
             "email",
             "cell_phone",
             "is_admin",
             "state",
             "address",
             "city",
-            "state",
+            "state_user",
             "service_provider",
             "service_user",
         ]
@@ -72,5 +40,39 @@ class UserSerializer(serializers.ModelSerializer):
             name = obj.email
         return name
 
-    def get_state(self, obj):
+    def get_state_user(self, obj):
         return obj.get_state_display()
+
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "name", "is_admin", "token"]
+
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token.access_token)
+
+
+class UserFullSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "first_name",
+            "last_name",
+            "cell_phone",
+            "email",
+            "password",
+            "cnpj",
+            "address",
+            "number",
+            "complement",
+            "postal_code",
+            "district",
+            "city",
+            "state",
+            "service_provider",
+            "service_user",
+        ]
